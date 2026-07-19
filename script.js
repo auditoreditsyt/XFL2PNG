c15f1b46303edc10e88b0ea7a2d248dd80c03bd1
auditoreditsyt <auditoreditsytextra@gmail.com>
2026-07-19 09:08:43 +0200

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
+function leerXML(){
 
+    const archivo =
+    document.getElementById("xmlFile").files[0];
+
+    if(!archivo){
+
+        alert("Selecciona un XML");
+
+        return;
+
+    }
+
+    const lector =
+    new FileReader();
+
+    lector.onload = function(e){
+
+        const parser =
+        new DOMParser();
+
+        const xml =
+        parser.parseFromString(
+            e.target.result,
+            "text/xml"
+        );
+
+        console.log(xml);
+
+        document.getElementById("salida").textContent =
+        e.target.result;
+
+    };
+
+    lector.readAsText(archivo);
+
+}
