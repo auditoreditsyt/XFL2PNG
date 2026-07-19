7f9944c012758b46bb319e4dcbfde622f6fabc9a
auditoreditsyt <auditoreditsytextra@gmail.com>
2026-07-19 09:32:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,39 @@
-function leerXML(){
+function leerXML() {
 
-    const archivo =
-    document.getElementById("xmlFile").files[0];
+    const archivo = document.getElementById("xmlFile").files[0];
 
-    if(!archivo){
-
+    if (!archivo) {
         alert("Selecciona un XML");
-
         return;
-
     }
 
-    const lector =
-    new FileReader();
+    const lector = new FileReader();
 
-    lector.onload = function(e){
+    lector.onload = function (e) {
 
-        const parser =
-        new DOMParser();
+        const parser = new DOMParser();
 
-        const xml =
-        parser.parseFromString(
+        const xml = parser.parseFromString(
             e.target.result,
             "text/xml"
         );
 
-        console.log(xml);
-
         const formas = xml.getElementsByTagName("DOMShape");
+        const colores = xml.getElementsByTagName("SolidColor");
 
-document.getElementById("salida").textContent =
-"DOMShape encontrados: " + formas.length;;
+        let texto = "";
+
+        texto += "DOMShape encontrados: " + formas.length + "\n";
+        texto += "Colores encontrados: " + colores.length + "\n\n";
+
+        for (let i = 0; i < colores.length; i++) {
+
+            texto += "Color " + (i + 1) + ": ";
+            texto += colores[i].getAttribute("color") + "\n";
+
+        }
+
+        document.getElementById("salida").textContent = texto;
 
     };
 
